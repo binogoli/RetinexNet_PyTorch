846897d567dd5b45a1c4f72e341f2855cc5ce88d
li <gcc_bingo@163.com>
2024-04-06 11:01:22 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ from glob import glob
 import numpy as np
 from model import RetinexNet
 import torch
+from PIL import Image
 import  torch.nn as nn
 
 parser = argparse.ArgumentParser(description='')
@@ -46,6 +47,59 @@ def test(model):
             model.test(image,
                 res_dir=args.res_dir,
                 ckpt_dir=args.ckpt_dir)
+            model.train_phase = 'Decom'
+            load_model_status, _ = model.load(args.ckpt_dir)
+            if load_model_status:
+                print(model.train_phase, "  : Model restore success!")
+            else:
+                print("No pretrained model to restore!")
+                raise Exception
+            model.train_phase = 'Relight'
+            load_model_status, _ = model.load(args.ckpt_dir)
+            if load_model_status:
+                print(model.train_phase, ": Model restore success!")
+            else:
+                print("No pretrained model to restore!")
+                raise Exception
+
+            # Set this switch to True to also save the reflectance and shading maps
+            save_R_L = False
+
+            # Predict for the test images
+
+            test_img_path = image
+            test_img_name = test_img_path.split('/')[-1]
+            print('Processing ', test_img_name)
+            test_low_img = Image.open(test_img_path)
+            test_low_img = np.array(test_low_img, dtype="float32") / 255.0
+            test_low_img = np.transpose(test_low_img, (2, 0, 1))
+            input_low_test = np.expand_dims(test_low_img, axis=0)
+
+            model.forward(input_low_test)
+            del input_low_test
+            result_4 = model.output_S
+            result_4 = np.squeeze(result_4)
+            # if save_R_L:
+            #     cat_image = np.concatenate([input, result_1, result_2, result_3, result_4], axis=2)
+            # else:
+            #     cat_image = np.concatenate([input, result_4], axis=2)
+            result_4 = np.concatenate([result_4], axis=2)
+
+            result_4 = np.transpose(result_4, (1, 2, 0))
+            # print(cat_image.shape)
+            im = Image.fromarray(np.clip(result_4 * 255.0, 0, 255.0).astype('uint8'))
+            del result_4
+
+            image_folder_name = test_img_path.split('/')[-2]
+            image_name = test_img_path.split("/")[-1]
+            folder_name = 'result/' + image_folder_name + '/'
+            result_path = folder_name + image_name
+
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
+
+            filepath = args.res_dir + '/' + test_img_path.split('/')[-2] + test_img_name
+            im.save(folder_name + image_name)
 
 if __name__ == '__main__':
     if args.gpu_id != "-1":
